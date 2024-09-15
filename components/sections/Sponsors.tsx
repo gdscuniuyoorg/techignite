53c67c8f3bf36c3552865a4c190e61f04931ab7c
AirZED <regnalukpabio@gmail.com>
2024-09-15 06:50:24 +0100

    working on sponsors

--- components/sections/Sponsors.tsx
+++ components/sections/Sponsors.tsx
@@ -3,14 +3,21 @@ import { sponsors } from "@/constants/partnersAndSponsors";
 
 const Sponsors = () => {
   return (
-    <PartnersAndSponsors
-      topic="Sponsors"
-      description="Meet our amazing Sponsors"
-      images={sponsors}
-      className="bg-[#130E09]"
-      link="https://docs.google.com/presentation/d/1VAC3zqAwdiqG3v464hNtmUDTz-KP9KWf1MSMy0l5-H8/edit?usp=sharing"
-      cta="Sponsor us"
-    />
+    <>
+      <div className="bg-heroPattern bg-[#130E09] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 py-14 lg:px-[130px] px-[1rem]">
+        {[...Array(15)].map((_, index) => (
+          <div key={index} className="bg-[#ccc] rounded-md h-40"></div>
+        ))}
+      </div>
+      <PartnersAndSponsors
+        topic="Sponsors"
+        description="Meet our amazing Sponsors"
+        images={sponsors}
+        className="bg-[#130E09]"
+        link="https://docs.google.com/presentation/d/1VAC3zqAwdiqG3v464hNtmUDTz-KP9KWf1MSMy0l5-H8/edit?usp=sharing"
+        cta="Sponsor us"
+      />
+    </>
   );
 };
 
